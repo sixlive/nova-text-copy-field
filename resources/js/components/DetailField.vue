a510e91155430c34ff34dc728246ef14bff94c4e
TJ Miller <sixlive@users.noreply.github.com>
2018-10-09 15:05:18 -0400

    Cliboard index support (#8)
    Adds support for the field on a resource index.
    
    Resolves #2

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -2,7 +2,7 @@
     <panel-item :field="field">
         <div slot="value" class="flex">
             <div class="flex-no-shrink">{{ field.value }}</div>
-            <copy-button :value="field.value" class="w-4 mx-4" />
+            <copy-button :value="field.value" class="w-4 mx-3" />
         </div>
     </panel-item>
 </template>
--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -1,9 +1,15 @@
 <template>
-    <span>{{ field.value }}</span>
+    <span class="flex">
+        <div class="flex-no-shrink">{{ field.value }}</div>
+        <copy-button :value="field.value" class="w-4 mx-3" />
+    </span>
 </template>
 
 <script>
+import CopyButton from './CopyButton'
+
 export default {
+    components: { CopyButton },
     props: ['resourceName', 'field'],
 }
 </script>
