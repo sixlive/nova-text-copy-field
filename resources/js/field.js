5c8eabf443cca082128307f29dd0e1099e36f31b
sixlive <tj@tjmiller.co>
2018-10-09 09:01:12 -0400

    Refactors cliboard to not use directives
    Resolves #5

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,8 +1,4 @@
-import VueClipboard from 'vue-clipboard2'
-
 Nova.booting((Vue, router) => {
-    Vue.use(VueClipboard)
-
     Vue.component('index-text-copy', require('./components/IndexField'));
     Vue.component('detail-text-copy', require('./components/DetailField'));
     Vue.component('form-text-copy', require('./components/FormField'));
