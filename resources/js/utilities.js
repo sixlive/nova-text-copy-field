a3a3f7ebde6018bf77e718622c185d0196da6c82
sixlive <tj@tjmiller.co>
2018-12-04 16:09:42 -0500

    Fixes error when field value is empty

--- resources/js/utilities.js
+++ resources/js/utilities.js
@@ -1,5 +1,5 @@
 export const fieldTruncator = function (field) {
-    return field.truncate > 0
+    return field.value && field.truncate > 0
         ?  `${field.value.substring(0, field.truncate)}...`
         : field.value
 }
